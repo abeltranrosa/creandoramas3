a97232e793b18be7c69b648bc72cc1ffc8e6684a
antonio <antonio@biruseto>
2016-10-21 11:47:01 +0200

    boletin5.1 main

---
+++ boletin5.1/src/boletin5/pkg1/Boletin51.java
@@ -0,0 +1,20 @@
+
+package boletin5.pkg1;
+
+import javax.swing.JOptionPane;
+
+
+public class Boletin51 {
+
+    
+    public static void main(String[] args) {
+        int a=Integer.parseInt(JOptionPane.showInputDialog("introduce numero"));
+         Positivo obxSimple = new Positivo();
+         obxSimple.compara(a);
+         
+        
+        
+              
+    }
+    
+}
