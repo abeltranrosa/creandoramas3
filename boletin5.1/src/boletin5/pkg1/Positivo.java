6db59bc88b207ce325b5c796855b5583a3ce08ed
antonio <antonio@biruseto>
2016-10-21 11:46:10 +0200

    boletin5.1 clase positivo

---
+++ boletin5.1/src/boletin5/pkg1/Positivo.java
@@ -0,0 +1,24 @@
+
+package boletin5.pkg1;
+
+
+public class Positivo {
+    private int num1;
+ 
+    
+    public Positivo(){
+ 
+    }
+    public Positivo(int n1){
+       num1= n1;
+      
+    }
+    public void compara (int n1){
+        if(n1>=0)
+            System.out.println("es positivo");
+           System.out.println("adios");
+    }
+}
+    
+    
+
