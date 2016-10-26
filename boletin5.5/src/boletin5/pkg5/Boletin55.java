eebf5079608f7d17eae78159c45c12d666fc20a0
antonio <antonio@192.168.0.16>
2016-10-26 13:19:46 +0200

    boletin 5.5

---
+++ boletin5.5/src/boletin5/pkg5/Boletin55.java
@@ -0,0 +1,20 @@
+
+package boletin5.pkg5;
+
+import java.util.Scanner;
+
+
+public class Boletin55 {
+
+   
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce el primer numero");
+        int n1 = sc.nextInt();
+        System.out.println("Introduce el segundo numero");
+        int n2 = sc.nextInt();
+        System.out.println("Introduce el tercer numero");
+        int n3 = sc.nextInt();
+    }
+    
+}
