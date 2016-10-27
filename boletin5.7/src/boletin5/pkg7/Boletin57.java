47ae02ad0c341e7f807807e0751c9cfe367f96f7
antonio <antonio@biruseto>
2016-10-27 11:21:04 +0200

    boletin5.7 main

---
+++ boletin5.7/src/boletin5/pkg7/Boletin57.java
@@ -0,0 +1,13 @@
+
+package boletin5.pkg7;
+
+
+public class Boletin57 {
+
+   
+    public static void main(String[] args) {
+        OpcionVisualizada opcion = new OpcionVisualizada();
+        opcion.menu();
+    }
+    
+}
